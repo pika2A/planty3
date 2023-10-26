51239520a038c38dfbfba3533feff655e3616559
Stephanie Salini <pika2a@hotmail.fr>
2023-10-26 11:13:42 +0200

    integration de la page nous rencontrer

--- app/public/wp-content/themes/blankslate-child/footer.php
+++ app/public/wp-content/themes/blankslate-child/footer.php
@@ -1,10 +1,9 @@
 </main>
-<?php get_sidebar(); ?>
-</div>
+
 <footer id="footer" role="contentinfo">
     <a href="http://planty3.local/?page_id=3">Mentions légales</a>
 </footer>
-</div>
+
 <?php wp_footer(); ?>
 </body>
 
