0556cd9b6a8bc91c7bbc6ce0c3f9f140548fc886
Stephanie Salini <pika2a@hotmail.fr>
2023-10-30 17:08:34 +0100

    mise en place de la bordure sur la page d'accueil et rendre les pages accueil et nous recontrer responsives

--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -18,6 +18,9 @@
 
             <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
                 <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
+
             </nav>
 
         </header>
+
+        <main>
