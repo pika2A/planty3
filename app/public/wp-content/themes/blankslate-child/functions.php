0510132f0d5558d2c005fa7dfaae0efd0bcf3912
Stephanie Salini <pika2a@hotmail.fr>
2023-11-06 16:29:15 +0100

    mise en place de la fonction ajouter lien admin

--- app/public/wp-content/themes/blankslate-child/functions.php
+++ app/public/wp-content/themes/blankslate-child/functions.php
@@ -25,3 +25,27 @@ add_action('wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10);
 
 
 // END ENQUEUE PARENT ACTION
+
+
+//fonction pour ajouter le lien admin quand connecté
+function ajouter_lien_admin($items, $args)
+{ //condition si  items (chaine de caractere qui contient les elements du menu) et args (objet qui contient info du menu)
+    if ( // si l’emplacement du thème est “main-menu”
+        $args->theme_location == 'main-menu'
+    ) { //si l'utilisateur est connecté
+        if (is_user_logged_in()) {
+            $admin_link = '<li class="admin"><a href="' . esc_url(admin_url()) . '">Admin</a></li>';
+            $items_array = explode('</li>', $items);
+            //La fonction explode est utilisée pour diviser la chaîne $items en un tableau d’éléments de menu
+            $middle_index = floor(count($items_array) / 2);
+            // floor est utilisée pour déterminer l’indice du milieu du tableau $items_array
+            array_splice($items_array, $middle_index, 0, $admin_link);
+            //array_splice est utilisée pour insérer le lien d’administration au milieu des deux autres liens sans suprimer les liens existant
+            $items = implode('</li>', $items_array);
+            //implode est utilisée pour fusionner les éléments de menu en une chaîne de caractères.
+        }
+    }
+    return $items;
+}
+//Ce filtre permet de modifier les éléments de menu avant qu’ils ne soient affichés
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin', 10, 2);
--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -15,7 +15,11 @@
             <a href="http://planty3.local/accueil/">
                 <img class="logo" src="http://planty3.local/wp-content/uploads/2023/10/Logo.png" alt="Logo de Planty">
             </a>
-
+            <!--
+            <button class="menu-toggle" aria-controls="menu" aria-expanded="false">
+                <span class="menu-toggle-icon"></span>
+            </button>
+-->
             <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
                 <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
 
@@ -24,3 +28,13 @@
         </header>
 
         <main>
+
+            <!-- <script type="text/javascript">
+                const menuToggle = document.querySelector(".menu-toggle");
+                const menuContainer = document.querySelector("#menu");
+
+                menuToggle.addEventListener("click", () => {
+                    menuToggle.classList.toggle("open");
+                    menuContainer.classList.toggle("open");
+                });
+            </script>-->
